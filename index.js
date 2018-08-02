197ddaf8db3d1332e495abc2db0c888f76dbd0d0
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-08-02 21:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0;
+  
+  function addBump() {
+    counter ++
+  }
+  
+  function getBumps() {
+    return counter;
+  }
+  
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+// closures Dangerous animals should have a `sharkWithFrickinLaserbeam` variable:
+var sharkWithFrickinLaserbeam = sharkCreator.deadlyDevice('Laserbeam');
+
+var sharkWithFrickinCannon = sharkCreator.deadlyDevice('Cannon');
+
+function createAnimal(animalType){
+  const animal = {
+    animalType: animalType,
+    deadlyDevice:  null
+  }
+  
+  return function(deadlyDevice){
+    return animal
+  }
+}
