5479ab3c242f298c4231a4840bfa9a66cfb19cc6
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-08-02 21:25:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function bumpCounter() {
 
 function createAnimal(animalType){
   return function(deadlyDevice){
-    return { animalType, deadlyDevice }
+    return { animalType: animalType, deadlyDevice: deadlyDevice }
   }
 }
 
